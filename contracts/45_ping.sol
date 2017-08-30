88ba52583295060fc6d092e34ef4bdad43d5b3ec
piouslove <305053045@qq.com>
2017-08-30 14:41:53 +0800

    Update 45_ping.sol

--- contracts/45_ping.sol
+++ contracts/45_ping.sol
@@ -1,11 +1,13 @@
+// 合约可以在其他合约里面得到非常量的函数返回值（而在 web3/geth 中则不行）
 // Contracts can get non-constant return values from functions in other contracts. (Whereas you can't from web3/geth.)
+// 以下用两个合约测试一下
 // These two contracts are meant to test this. Like so:
 
-// 1. Deploy Pong with a pongval.
-// 2. Deploy Ping, giving it the address of Pong.
-// 3. Call Ping.getPongvalRemote() using a sendTransaction...
-// 4. ... which retreives the value of pongval from Pong.
-// 5. If successful Ping.getPongval() should return the value from step 1.
+// 1. Deploy Pong with a pongval. 先传入pongval值部署Pong合约，也就是Pong合约有pongval值
+// 2. Deploy Ping, giving it the address of Pong. 部署Ping合约，并把Pong的地址发送给它
+// 3. Call Ping.getPongvalRemote() using a sendTransaction... 用一个交易调用Ping.getPongvalRemote()函数
+// 4. ... which retreives the value of pongval from Pong. Ping从Pong合约取回pongval值
+// 5. If successful Ping.getPongval() should return the value from step 1. 如果成功Ping.getPongval()应该返回步骤1的值
 
 contract PongvalRetriever {
  	int8 pongval_tx_retrieval_attempted = 0;
